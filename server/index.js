925e67484148344ade3c6842deebf639aa1b26a7
nmontiel <58341731+nemontiel@users.noreply.github.com>
2022-01-07 15:07:57 -0300

    index

--- server/index.js
+++ server/index.js
@@ -1,6 +1,4 @@
 const express = require("express");
 const app = express();
 
-const server = app.listen("4000", () =>
-  console.log("Server listening on port 4000")
-);
+app.listen("4000", () => console.log("Server listening on port 4000"));
