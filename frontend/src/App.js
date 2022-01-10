be61dd9c8fd5cf8557ce2cfcee96c6c06ccfa4d3
Nicolas Catriel Cirulli <nicolascirulli2@gmail.com>
2022-01-10 15:25:52 -0300

    redux, rutas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,23 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from "./pages/Home"
-import Navigation from "./components/Navigation"
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/Home";
+import Navigation from "./components/Navigation";
 function App() {
   return (
-    <div className="App">
-        <Navigation />
-        <Home />
-        
-    </div>
+    <>
+      <BrowserRouter>
+        <div className="App">
+          <Navigation />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="*" element={<Home />}></Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './App.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware,compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import mainReducer from "./redux/reducers/mainReducer";
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const reduxStore = createStore(mainReducer,composeEnhancer(applyMiddleware( thunk )));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={reduxStore}>
     <App />
-  </React.StrictMode>,
+    </Provider>,
   document.getElementById('root')
 );
 
