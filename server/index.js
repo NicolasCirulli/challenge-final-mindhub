5aaf8cef85f359fed7c6312021564b2f746fbd09
nmontiel <58341731+nemontiel@users.noreply.github.com>
2022-01-10 00:04:21 -0300

    user model

--- server/index.js
+++ server/index.js
@@ -1,4 +1,10 @@
 const express = require("express");
 const app = express();
+const router = require("./routes/routes");
 
+require("dotenv").config();
+require("./config/database");
+
+app.use(express.json());
+app.use("/api", router);
 app.listen("4000", () => console.log("Server listening on port 4000"));
