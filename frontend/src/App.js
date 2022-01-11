fd6a87bbc6bb07c9c1d19b66b38c9fcac071a6d4
Maca-Acha <achamariamacarena@gmail.com>
2022-01-11 15:58:38 -0300

    page about - incompleta

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home";
 import Navigation from "./components/Navigation";
+import About from "./pages/About"
+
 function App() {
   return (
     <>
@@ -14,6 +15,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="*" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
           </Routes>
         </div>
       </BrowserRouter>
