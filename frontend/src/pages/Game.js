d5969d6c53e93fff4069657a032e2edce87ac90d
MarTakaes <maaritakaes@gmail.com>
2022-01-11 16:45:23 -0300

    Prueba

--- frontend/src/pages/Game.js
+++ frontend/src/pages/Game.js
@@ -3,6 +3,9 @@ import "../styles/game.css"
 export default function Game () {
     return (
         <>
+            <div className="covergame">
+                
+            </div>
             <div className="container">
                 <h1 className="gamePath">STORE/</h1>
             </div>
