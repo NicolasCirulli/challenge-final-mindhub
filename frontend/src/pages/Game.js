cbee18add5c629202e7118e7ff9da4f0eb8f3d86
MarTakaes <maaritakaes@gmail.com>
2022-01-11 17:17:33 -0300

    Feat: upload changes

--- frontend/src/pages/Game.js
+++ frontend/src/pages/Game.js
@@ -1,11 +1,24 @@
 import "../styles/game.css"
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { useState } from "react";
+import IconButton from "@mui/material/IconButton";
 
 export default function Game () {
+    const [like, setlike] = useState(false)
     return (
         <>
-            <div className="container">
-                <h1 className="gamePath">STORE/</h1>
+            <div className="gamebg">
+                <div className="container">
+                    <h1 className="gamePath">STORE/</h1>
+                </div>
+                <div>
+                    <h2 className="gametitle">Nombre del Juego</h2>
+                    <IconButton aria-label="add to favorites" className="fav" onClick={() => setlike(!like)}>
+                        {like? <FavoriteIcon className="favorite"/> : <FavoriteIcon className="favorite2"/>}
+                    </IconButton>
+                </div>
             </div>
+            
         </>
     )
 }
