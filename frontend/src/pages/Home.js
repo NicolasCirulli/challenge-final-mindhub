c70e1b09b5e9bdaf4b21718237a9b24f8a02e7e8
Maca-Acha <achamariamacarena@gmail.com>
2022-01-12 09:14:19 -0300

    logo en home y correccion hover en botones

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,6 +4,7 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import LocalGroceryStoreIcon from "@mui/icons-material/LocalGroceryStore";
 import CardGames from "../components/CardGames";
+import logo from "../assets/logo.png"
 
 const DrawerHeader = styled("div")(({ theme }) => ({
     display: "flex",
@@ -17,13 +18,16 @@ const DrawerHeader = styled("div")(({ theme }) => ({
 export default function Home() {
     return (
         <div className="container">
-            <div className="cont-search">
-                <input
-                    type="text"
-                    className="search"
-                    placeholder="Search a Game"
-                />
-                <button className="btn-search">Search</button>
+            <div className="container header">
+                <div className="cont-search">
+                    <input
+                        type="text"
+                        className="search"
+                        placeholder="Search a Game"
+                    />
+                    <button className="btn-search">Search</button>
+                </div>
+                <img src={logo} className="logo-home" />
             </div>
             <div className="container Recommended">
                 <Typography
