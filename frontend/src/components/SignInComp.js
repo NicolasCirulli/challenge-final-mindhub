a2638ba5bf30ed5b79b16e1a5d38ef10b49837a9
MarTakaes <maaritakaes@gmail.com>
2022-01-13 02:03:43 -0300

    Fix: agrego detalles de signs (reemplazo de algunas palabras)

--- frontend/src/components/SignInComp.js
+++ frontend/src/components/SignInComp.js
@@ -39,7 +39,7 @@ const SignInComp = () => {
             <div className="cardSign">
                 <div className='contentSignIn'>
                 <h1 className='titleSignIn'>It's great to see you again</h1>
-                    <h2>Sign in Xtreme</h2>
+                    <h2>Sign in xtreme</h2>
                     <form>
                         <div className='inputsSignIn'>
                             <input type="text" className='label-SI' placeholder=' Email' ref={email} />
@@ -57,9 +57,9 @@ const SignInComp = () => {
                 >
                     <div  className="parrSignUp">
                     <h1>Hello!</h1>
-                    <p>Welcome again to our site. We have so many captivating projects for
-                 you. Just sign up and visit our app store with different selections. And don't forget to share your
-                 achievements with!
+                    <p>Welcome again to our site. We have so many captivating games for
+                 you. Just sign up and visit our game store with different selections. And don't forget to share your
+                 opinions with!
                     </p>
                     </div>
                 </div>
