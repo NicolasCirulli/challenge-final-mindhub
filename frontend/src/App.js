92a1f5227d95e990fc49cb525660f3dae29b75a7
MarTakaes <maaritakaes@gmail.com>
2022-01-14 01:31:41 -0300

    Feat: starting profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import Profile from "./pages/Profile"
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home";
@@ -16,6 +16,7 @@ function App() {
             <Route path="/" element={<Home />}></Route>
             <Route path="/game" element={<Game/>}></Route>
             <Route path="*" element={<Home />}></Route>
+            <Route path="profile" element={<Profile/>}></Route>
           </Routes>
         </div>
       </BrowserRouter>
