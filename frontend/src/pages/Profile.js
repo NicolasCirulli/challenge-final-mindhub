d297d3e35cec9c9f85626aa83619271c5c246441
MarTakaes <maaritakaes@gmail.com>
2022-01-14 01:56:14 -0300

    Feat: merge completo, corregir validator

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -6,6 +6,9 @@ export default function Profile (){
                 <div className="container profile">
                     <h1 className="profile-title">PROFILE</h1>
                 </div>
+                <div className="profile-img-div">
+                   <img className="profile-img" src="../assets/gtav.jpg"/>
+                </div>
         </>
     )
 }
