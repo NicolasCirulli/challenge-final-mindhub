290128309ccac6e31548df3bec996d325ebce159
Nicolas Catriel Cirulli <nicolascirulli2@gmail.com>
2022-01-14 22:09:03 -0300

    fix : ahora si loggea cuando creas cuenta

--- frontend/src/redux/actions/authActions.js
+++ frontend/src/redux/actions/authActions.js
@@ -8,11 +8,12 @@ const usuarioActions = {
 
                 const user = await axios.post('http://localhost:4000/api/user/signup',{firstName, lastName, userName, mail, password, image, address})
                 if(user.data.success){
-                    localStorage.setItem('token',user.data.response.token)
-                    dispatch({type:'signIn', payload:{userName: user.data.response.userName, image:user.data.response.image, id: user.data.response.id}})
+                    localStorage.setItem('token',user.data.res.token)
+                    dispatch({type:'signIn', payload:{userName: user.data.res.userName, image:user.data.res.image, id: user.data.res.id}})
                     return user.data
                 }
                 return user.data
+               
 
             }catch(err){console.log(err)}
            
