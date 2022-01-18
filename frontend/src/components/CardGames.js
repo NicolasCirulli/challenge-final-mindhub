d675d2cacabed9fbd11750fa1e58e05d058d97d4
Maca-Acha <achamariamacarena@gmail.com>
2022-01-18 20:31:40 -0300

    saque cosas comentadas

--- frontend/src/components/CardGames.js
+++ frontend/src/components/CardGames.js
@@ -6,27 +6,29 @@ import Favorite from "@mui/icons-material/Favorite";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import authActions from "../redux/actions/authActions";
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
 
 export default function RecipeReviewCard({ game }) {
     const dispatch = useDispatch();
     const user = useSelector((store) => store.userReducer.user);
 
     const handleFavs = () => {
-        user ? dispatch(authActions.wishList(game._id)) : Swal.fire({
-            toast: true,
-            position: 'top-end',
-            icon: 'warning',
-            iconColor: '#af3181',
-            showConfirmButton: false,
-            timer: 3000,
-            title: 'You must log in to be able to like!',
-            timerProgressBar: true,
-            didOpen: toast => {
-                toast.addEventListener('mouseenter', Swal.stopTimer)
-                toast.addEventListener('mouseleave', Swal.resumeTimer)
-            }
-        })
+        user
+            ? dispatch(authActions.wishList(game._id))
+            : Swal.fire({
+                toast: true,
+                position: "top-end",
+                icon: "warning",
+                iconColor: "#af3181",
+                showConfirmButton: false,
+                timer: 3000,
+                title: "You must log in to be able to like!",
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener("mouseenter", Swal.stopTimer);
+                    toast.addEventListener("mouseleave", Swal.resumeTimer);
+                },
+            });
     };
 
     return (
@@ -35,27 +37,26 @@ export default function RecipeReviewCard({ game }) {
                 className="card-img"
                 style={{ backgroundImage: `url("${game.background_image}")` }}
             >
-                
-                    <IconButton
-                        aria-label="add to favorites"
-                        className="fav"
-                        onClick={handleFavs}
-                    >
-                        <Checkbox
-                            // icon={<FavoriteBorder />}
-                            // checkedIcon={<Favorite className="favorite"/>}
-                            icon={user ? (
+                <IconButton
+                    aria-label="add to favorites"
+                    className="fav"
+                    onClick={handleFavs}
+                >
+                    <Checkbox
+                        icon={
+                            user ? (
                                 !user.wishList.includes(game._id) ? (
                                     <FavoriteBorder />
                                 ) : (
                                     <Favorite className="favorite" />
                                 )
+                            ) : (
+                                <FavoriteBorder />
+                            )
+                        }
+                    />
+                </IconButton>
 
-                            ):<FavoriteBorder />
-                            }
-                        />
-                    </IconButton>
-                
                 <div className="card-title">
                     <Link className="name-game " to={`/game/${game._id}`}>
                         {game.name}
