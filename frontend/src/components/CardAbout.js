a29761411f232dd8280de1c86fed4b10ad38034e
Maca-Acha <achamariamacarena@gmail.com>
2022-01-20 01:05:06 -0300

    responsive about

--- frontend/src/components/CardAbout.js
+++ frontend/src/components/CardAbout.js
@@ -20,70 +20,72 @@ export default function CardAbout() {
                     features such as:
                 </h5>
             </div>
-            <div className="cont-card-about">
-                <div className="card-about">
-                    <div className="icon-card-about">
-                        <ChatBubbleOutlineIcon />
+            <div className="cont-cont-card">
+                <div className="cont-card-about">
+                    <div className="card-about">
+                        <div className="icon-card-about">
+                            <ChatBubbleOutlineIcon />
+                        </div>
+                        <h5 className="title-about">Chat</h5>
+                        <p className="text-about">
+                            Communicate with the administrators so that they give
+                            you all the information you need.
+                        </p>
                     </div>
-                    <h5 className="title-about">Chat</h5>
-                    <p className="text-about">
-                        Communicate with the administrators so that they give
-                        you all the information you need.
-                    </p>
+                    <div className="card-about">
+                        <div className="icon-card-about">
+                            <InfoOutlinedIcon />
+                        </div>
+                        <h5 className="title-about">Information</h5>
+                        <p className="text-about">
+                            Know everything you need about your favorite game. Find
+                            more information in the reviews and comments.
+                        </p>
+                    </div>
+                    <div className="card-about">
+                        <div className="icon-card-about">
+                            <PhoneAndroidOutlinedIcon />
+                        </div>
+                        <h5 className="title-about">
+                            Available for mobile devices
+                        </h5>
+                        <p className="text-about">
+                            Access Xtreme anywhere from your IOS or Android device.
+                        </p>
+                    </div>
                 </div>
-                <div className="card-about">
-                    <div className="icon-card-about">
-                        <InfoOutlinedIcon />
+                <div className="cont-card-about">
+                    <div className="card-about">
+                        <div className="icon-card-about">
+                            <FontDownloadOutlinedIcon />
+                        </div>
+                        <h5 className="title-about">Multilingual</h5>
+                        <p className="text-about">
+                            Creating a global community is important to us, which is
+                            why our client supports multiple languages.
+                        </p>
                     </div>
-                    <h5 className="title-about">Information</h5>
-                    <p className="text-about">
-                        Know everything you need about your favorite game. Find
-                        more information in the reviews and comments.
-                    </p>
-                </div>
-                <div className="card-about">
-                    <div className="icon-card-about">
-                        <PhoneAndroidOutlinedIcon />
+                    <div className="card-about">
+                        <div className="icon-card-about">
+                            <CreditCardOutlinedIcon />
+                        </div>
+                        <h5 className="title-about">Simplified purchases</h5>
+                        <p className="text-about">
+                            Our store supports several payment methods, giving you
+                            the flexibility to pay how you want.
+                        </p>
                     </div>
-                    <h5 className="title-about">
-                        Available for mobile devices
-                    </h5>
-                    <p className="text-about">
-                        Access Xtreme anywhere from your IOS or Android device.
-                    </p>
-                </div>
-            </div>
-            <div className="cont-card-about">
-                <div className="card-about">
-                    <div className="icon-card-about">
-                        <FontDownloadOutlinedIcon />
+                    <div className="card-about">
+                        <div className="icon-card-about">
+                            <VideogameAssetOutlinedIcon />
+                        </div>
+                        <h5 className="title-about">Controller Compatibility</h5>
+                        <p className="text-about">
+                            Xtreme encourages developers to include controller
+                            support in their games, including PlayStation, Xbox, and
+                            Nintendo controllers.
+                        </p>
                     </div>
-                    <h5 className="title-about">Multilingual</h5>
-                    <p className="text-about">
-                        Creating a global community is important to us, which is
-                        why our client supports multiple languages.
-                    </p>
-                </div>
-                <div className="card-about">
-                    <div className="icon-card-about">
-                        <CreditCardOutlinedIcon />
-                    </div>
-                    <h5 className="title-about">Simplified purchases</h5>
-                    <p className="text-about">
-                        Our store supports several payment methods, giving you
-                        the flexibility to pay how you want.
-                    </p>
-                </div>
-                <div className="card-about">
-                    <div className="icon-card-about">
-                        <VideogameAssetOutlinedIcon />
-                    </div>
-                    <h5 className="title-about">Controller Compatibility</h5>
-                    <p className="text-about">
-                        Xtreme encourages developers to include controller
-                        support in their games, including PlayStation, Xbox, and
-                        Nintendo controllers.
-                    </p>
                 </div>
             </div>
         </div>
